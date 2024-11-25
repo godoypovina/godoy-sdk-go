192a0b36651531a333330793e7f128a86d40668e
Marcelo Agustini <marceloagustini@MacBook-Air-de-Marcelo.local>
2024-11-25 09:03:51 -0300

    feat:saved

--- godoy.go
+++ godoy.go
@@ -15,7 +15,7 @@ import (
 const (
 	userAgent string = "Godoy Go SDK"
 	//apiURL   string = "https://api.godoypovina.com.ar"
-	//apiURL   string = "http://localhost:5000"
+	//apiURL string = "http://localhost:5000"
 	// apiURL   string = "http://192.168.1.238:5005"
 	apiURL   string = "http://godoy-api:80"
 	version  string = "v1"
